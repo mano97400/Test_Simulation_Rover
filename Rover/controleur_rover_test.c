d221b00f093265bc908a569ef26d451b49f42601
LEPERLIER Manoel <leperliermanoel@MacBook-Air-de-LEPERLIER.local>
2023-01-10 13:39:45 +0100

    sans commentaire

--- Rover/controleur_rover_test.c
+++ Rover/controleur_rover_test.c
@@ -104,7 +104,7 @@ int main ( void )
    CU_list_tests_to_file();
 
    // Run all tests using the console interface
-   CU_console_run_tests();
+   //CU_console_run_tests();
 
    /* Clean up registry and return */
    CU_cleanup_registry();
