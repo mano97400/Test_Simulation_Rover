6694e3bec073721def27c890434a1edb8e6d6693
LEPERLIER Manoel <leperliermanoel@MacBook-Air-de-LEPERLIER.local>
2023-01-10 13:38:36 +0100

    sans commentaire

--- Rover/controleur_rover_test.c
+++ Rover/controleur_rover_test.c
@@ -93,19 +93,19 @@ int main ( void )
    }
 
    // Run all tests using the basic interface
-   CU_basic_set_mode(CU_BRM_VERBOSE);
-   CU_basic_run_tests();
-   printf("\n");
-   CU_basic_show_failures(CU_get_failure_list());
-   printf("\n\n");
-/*
+   // CU_basic_set_mode(CU_BRM_VERBOSE);
+   // CU_basic_run_tests();
+   // printf("\n");
+   // CU_basic_show_failures(CU_get_failure_list());
+   // printf("\n\n");
+
    // Run all tests using the automated interface
    CU_automated_run_tests();
    CU_list_tests_to_file();
 
    // Run all tests using the console interface
    CU_console_run_tests();
-*/
+
    /* Clean up registry and return */
    CU_cleanup_registry();
    return CU_get_error();
