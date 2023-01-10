e8a1ee9eb3f0bb98850c7460b8c6840444c0faa0
mano97400 <109896493+mano97400@users.noreply.github.com>
2023-01-10 12:16:21 +0100

    Update controleur_rover.c

--- Rover/controleur_rover.c
+++ Rover/controleur_rover.c
@@ -17,7 +17,7 @@
 int batterie = 100;
 bool retour_point_de_depart=false;
 float pas_echantillonage;
-int daronne=50;
+int tadaronne=50;
 
 OutputControler controleur(Capteurs capteurs,bool button_emergency, int batterie){
     float output_speed_motor[6];
